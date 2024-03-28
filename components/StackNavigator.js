e29c3efc979e3eaf66f3b910ec9818034d2eaa0d
YIJING WU <yijingwu@YIJINGdeMacBook-Pro.local>
2024-03-27 17:15:25 -0700

    moved the Login and Signup to screens folder and made it connect to firebase, modified the StackNavigator

--- components/StackNavigator.js
+++ components/StackNavigator.js
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import StartScreen from "../screens/StartScreen";
+import SignupScreen from "../screens/SignupScreen";
+import LoginScreen from "../screens/LoginScreen";
 import TabNavigator from "./TabNavigator";
+import { colors } from "../helper/Color";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../firebase-files/firebaseSetup";
 
 const Stack = createStackNavigator();
 
 export default function StackNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Start"
-        component={StartScreen}
-        options={{ headerShown: false }}
-      />
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserLoggedIn(true);
+      } else {
+        setUserLoggedIn(false);
+      }
+    });
+  }, []);
+
+  const AuthStack = (
+    <>
+      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+    </>
+  );
+
+  const AppStack = (
+    <>
       <Stack.Screen
         name="TabNavigator"
         component={TabNavigator}
         options={{ headerShown: false }}
       />
+    </>
+  );
+
+  return (
+    <Stack.Navigator
+      initialRouteName="Signup"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      {userLoggedIn ? AppStack : AuthStack}
     </Stack.Navigator>
   );
 }
