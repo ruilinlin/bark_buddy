aa5dc5e63ab4211da73cb2d15419878a63b500d2
YIJING WU <yijingwu@YIJINGdeMacBook-Pro.local>
2024-04-09 23:57:34 -0700

    replaced the hard coded User info in UserScreen to the info fetched from database

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -11,14 +11,45 @@ import {
   ScrollView,
   Dimensions,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LightBackGround from "../components/LightBackGround";
 import { colors } from "../helper/Color";
 import PostItem from "../components/PostItem";
 import ImageViewer from "../components/PostImageViewer";
 // import LinearGradient from 'react-native-linear-gradient';
+import { auth } from "../firebase-files/firebaseSetup";
+import {
+  searchUsersByUserId,
+  writeToDB,
+  updateToDB,
+} from "../firebase-files/firestoreHelper";
 
 export default function UserScreen() {
+  const [name, setName] = useState("");
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [docId, setDocId] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const userData = await searchUsersByUserId(auth.currentUser.uid); // Fetch user data from DB
+        if (userData) {
+          // If user data exists, pre-fill the input fields
+          setName(userData.name);
+          setCountry(userData.country);
+          setState(userData.state);
+          setCity(userData.city);
+          setDocId(userData.id);
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
   const user = {
     id: "1",
     name: "test",
@@ -31,39 +62,6 @@ export default function UserScreen() {
     ],
   };
 
-  const comments = [
-    {
-      id: "1",
-      name: "test",
-      avatar: require("../assets/favicon.png"),
-      comments: "Mobile Application Development SEC 05 Spring 2024 ",
-    },
-    {
-      id: "2",
-      name: "test",
-      avatar: require("../assets/favicon.png"),
-      comments: "Mobile Application Development SEC 05 Spring 2024 ",
-    },
-    {
-      id: "3",
-      name: "test",
-      avatar: require("../assets/favicon.png"),
-      comments: "Mobile Application Development SEC 05 Spring 2024 ",
-    },
-  ];
-
-  const stories = [
-    {
-      id: "1",
-      username: "iiamcharlie",
-      avatar: require("../assets/favicon.png"),
-    },
-    {
-      id: "2",
-      username: "iiamcharles",
-      avatar: require("../assets/favicon.png"),
-    },
-  ];
   const images = [
     { id: "1", uri: require("../assets/1.png") },
     { id: "2", uri: require("../assets/2.png") },
@@ -97,8 +95,8 @@ export default function UserScreen() {
               resizeMode="cover"
             />
           </Pressable>
-          <Text style={styles.Username}>{user.name}</Text>
-          <Text style={styles.location}>{user.livein}</Text>
+          <Text style={styles.Username}>{name}</Text>
+          <Text style={styles.location}>{`${country}, ${state}, ${city}`}</Text>
         </View>
         <View style={styles.titleContainer}>
           <Text style={styles.titleText}>Puppy is Here</Text>
