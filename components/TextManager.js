73f5e06e60ef9ad95b94d3f93c8b45f66b1e59f9
ruilinsun <garrolinlin@gmail.com>
2024-04-11 20:22:04 -0700

    change style

--- components/TextManager.js
+++ components/TextManager.js
@@ -125,7 +125,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.lightbackgroundlight,
-    padding: 10
+    // padding: 10
   },
   image: {
     width: 400,
