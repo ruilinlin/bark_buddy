3436cfc193308a2799896bf005bdf6de016d36aa
ruilinsun <garrolinlin@gmail.com>
2024-04-20 13:52:05 -0700

    fix the screen switch logic problema

--- components/TextManager.js
+++ components/TextManager.js
@@ -8,6 +8,8 @@ import { auth, database, storage } from "../firebase-files/firebaseSetup";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { writeToDB } from "../firebase-files/firestoreHelper";
 import LottieView from 'lottie-react-native';
+import ImageViewer from './PostImageViewer';
+
 export default function TextManager({ navigation, route }) {
   const [images, setImages] = useState([]);
   const [description, setDescription] = useState('');
@@ -15,10 +17,14 @@ export default function TextManager({ navigation, route }) {
 
   useEffect(() => {
     if (route.params?.images) {
-      setImages(route.params.images.map(img => img.uri)); 
+      setImages(route.params.images);
     }
+    // else(){
+    //   console.log("ask permission again");
+    // }
   }, [route.params?.images]);
 
+  console.log(images)
   // Upload image to Firebase Storage and return the URL
   async function uploadImage(uri) {
     try {
@@ -76,9 +82,10 @@ export default function TextManager({ navigation, route }) {
   return (
     <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }}>
       <ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
-        {images.map((img, index) => (
+      <ImageViewer images={images} />
+        {/* {images.map((img, index) => (
           <Image key={index} source={{ uri: img }} style={styles.image} resizeMode="cover" />
-        ))}
+        ))} */}
         {/* <FloatingWindow navigation={navigation} /> */}
         <Input
           label="Description"
