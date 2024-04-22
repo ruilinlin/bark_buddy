df5e1959166cd7b376766f34ea60f4035a60c56c
ruilinlin <106147018+ruilinlin@users.noreply.github.com>
2024-04-21 23:43:53 -0700

    change back to filter page to back image page

--- components/TextManager.js
+++ components/TextManager.js
@@ -69,7 +69,7 @@ export default function TextManager({ navigation, route }) {
   };
 
   function handleBack(){
-    navigation.navigate('Filter');
+    navigation.navigate('Album');
   }
 
   return (
